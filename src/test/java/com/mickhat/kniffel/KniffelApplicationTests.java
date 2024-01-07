c525fbd9c554f09e9201e3869c4dde3886387fac
Mickhat <root@mickhat.xyz>
2024-01-07 19:07:17 +0100

    first commit

---
+++ src/test/java/com/mickhat/kniffel/KniffelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mickhat.kniffel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KniffelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
